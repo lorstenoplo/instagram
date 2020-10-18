1e699cb3e06e749d04b385941663e4dd4f49d3f1
nishanth <gayatridipali@gmail.com>
2020-10-18 17:04:25 +0530

    init

---
+++ precache-manifest.32858c59f7d164c0990f5c78e0a85ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6f01169853800251e2ebae373d0d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dbdaeb94bc4bbe239da",
+    "url": "/static/css/main.1c5cb576.chunk.css"
+  },
+  {
+    "revision": "7541cabc29f26059bc92",
+    "url": "/static/js/2.74ad4cad.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.74ad4cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbdaeb94bc4bbe239da",
+    "url": "/static/js/main.2c429090.chunk.js"
+  },
+  {
+    "revision": "97dd0eeb3304547d5553",
+    "url": "/static/js/runtime-main.05223113.js"
+  }
+]);
